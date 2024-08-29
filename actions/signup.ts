c7c805c958b989d68b4a3ce3fc1ee657961ee899
dmycse <dmycse@gmail.com>
2024-08-29 17:49:48 +0300

    Refactor(actions): improve signup action

--- actions/signup.ts
+++ actions/signup.ts
@@ -3,8 +3,14 @@
 import { AuthFormSchema } from "@/schemas";
 import type { AuthFormType } from '@/schemas';
 
+import { db } from '@/lib/db';
+import { getUserByEmail } from '../utils/user';
 
-export const signup = async(formData: AuthFormType) => {
+import bcrypt from 'bcrypt';
+let saltRounds = 10;
+
+
+export const signup = async (formData: AuthFormType) => {
   console.log('SignUp action formData: ', formData);
 
   let validatedResult = AuthFormSchema.safeParse(formData);
@@ -12,6 +18,25 @@ export const signup = async(formData: AuthFormType) => {
   if (!validatedResult.success) {
     return {error: 'Your credentials invalid. Try again.'};
   }
+  console.log('validatedResult', validatedResult);
 
-  return {success: 'Your credentials sent!'};
+  let {name, email, password} = validatedResult.data;
+
+  let emailExists = await getUserByEmail(email);
+  
+  if (emailExists) {
+    return {error: 'Email is already taken!'};
+  }
+
+  let hashedPassword = await bcrypt.hash(password, saltRounds);
+
+  await db.user.create({
+    data: {
+      name,
+      email,
+      password: hashedPassword 
+    },
+  });
+
+  return {success: 'User created!'};
 }
---
+++ utils/user.ts
@@ -0,0 +1,26 @@
+import { db } from '../lib/db';
+
+
+export let getUserByEmail = async (email: string) => {
+  try {
+
+    let user = await db.user.findUnique({where: { email }});
+
+    return user;
+    
+  } catch {
+      return null
+  }
+};
+
+export let getUserById = async (id: string) => {
+  try {
+
+    let user = await db.user.findUnique({where: { id }});
+
+    return user;
+    
+  } catch {
+      return null
+  }
+};
