b9cf933ce0e5ddd108f19dbc2507bdb78ed6c07c
dmycse <dmycse@gmail.com>
2024-08-31 10:49:11 +0300

    Refactor(zod): refactor schema

--- schemas/index.ts
+++ schemas/index.ts
@@ -15,17 +15,7 @@ export const SignUpFormSchema = z.object({
 
 export type SignUpFormType = z.infer<typeof SignUpFormSchema>;
 
-
-export const SignInFormSchema = z.object({
-  email: z.string()
-            .min(1, 'Email is required')
-            .email("Invalid email"),
-            
-  password: z.string()
-            .min(1, "Password is required")
-            .min(6, "Password must be at least 6 characters")
-  });
-
+export const SignInFormSchema = SignUpFormSchema.omit({name: true});
 export type SignInFormType = z.infer<typeof SignInFormSchema>;
 
 
