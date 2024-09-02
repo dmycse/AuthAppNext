bff444a90347960a21c9023045dfb75229fe5adb
dmycse <dmycse@gmail.com>
2024-09-02 10:46:33 +0300

    Feat(auth.config): create credential provider

--- auth.config.ts
+++ auth.config.ts
@@ -1,6 +1,24 @@
-import GitHub from "next-auth/providers/github";
 import type { NextAuthConfig } from "next-auth";
+import Credentials from 'next-auth/providers/credentials';
+// import GitHub from "next-auth/providers/github";
+
+import { SignInFormSchema } from '@/schemas';
+
+import { mathcUserPassword } from '@/utils/pass';
  
 export default { 
-  providers: [GitHub] 
+  // providers: [GitHub]
+  providers: [
+   Credentials({
+    async authorize(credentials) {
+      let isAuthData = SignInFormSchema.safeParse(credentials);
+      
+      if (isAuthData.success) {
+        return await mathcUserPassword(isAuthData.data);
+      }
+
+      return null;
+    },
+   }) 
+  ] 
 } satisfies NextAuthConfig;
