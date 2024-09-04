160fdad00ce9c8c75e36b242fccc63018c2a008d
dmycse <dmycse@gmail.com>
2024-09-04 14:37:29 +0300

    Feat(components): add onclick handlers to social signin buttons

--- components/custom_ui/Social.tsx
+++ components/custom_ui/Social.tsx
@@ -1,18 +1,29 @@
 'use client';
 
+import { signIn } from "next-auth/react";
+
 import { FcGoogle } from "react-icons/fc";
 import { FaGithub } from "react-icons/fa";
 import { Button } from "@/components/ui/button";
+import { DEFAULT_SIGNIN_REDIRECT } from "@/routes";
 
 
 export const Social = () => {
+
+  let onClickHandler = (provider: "google" | "github") => {
+    signIn(provider, {
+      callbackUrl: DEFAULT_SIGNIN_REDIRECT
+    })
+  };
+  
+
   return (
     <div className='w-full flex items-center space-x-2'>
       <Button 
         size="lg" 
         variant="outline" 
         className="w-full"
-        onClick={() => {}}
+        onClick={() => onClickHandler("google")}
       >
         <FcGoogle className="w-5 h-5" />
       </Button>
@@ -20,7 +31,7 @@ export const Social = () => {
         size="lg" 
         variant="outline" 
         className="w-full"
-        onClick={() => {}}
+        onClick={() => onClickHandler("github")}
       >
         <FaGithub className="w-5 h-5" />
       </Button>
