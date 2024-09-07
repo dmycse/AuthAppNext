1ee4cbf6792c0e53ad6a52179dff3d43c468bb41
dmycse <dmycse@gmail.com>
2024-09-07 12:48:15 +0300

    Feat(app): add signout feature to settings page

--- app/(protected)/settings/page.tsx
+++ app/(protected)/settings/page.tsx
@@ -1,12 +1,22 @@
-import React from "react";
-import { auth } from "../../../auth";
+import { auth, signOut } from "@/auth";
 
 export default async function SettingsPage() {
   let session = await auth();
+  console.log("\x1b[34m", 'SETTINGSPAGE session: ', "\x1b[0m", session);
+
+  let signOutAction = async () => {
+    "use server";
+    await signOut();
+  };
   
   return (
     <div>
       {JSON.stringify(session)}
+      <form action={signOutAction}>
+        <button type="submit" className="border">
+          Sign Out
+        </button>
+      </form>
     </div>
   )
 }
