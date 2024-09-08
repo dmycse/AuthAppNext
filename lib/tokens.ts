6b2cfe245bd82c6acb15386f27811fc647bd38a1
dmycse <dmycse@gmail.com>
2024-09-08 13:10:04 +0300

    Feat(lib): create generatePasswordResetToken

--- lib/tokens.ts
+++ lib/tokens.ts
@@ -1,6 +1,7 @@
 import { v4 as uuidv4 } from "uuid";
 import { db } from "@/lib/db";
 import { getVerificationTokenByEmail } from "@/utils/verificationtoken";
+import { getPasswordResetTokenByEmail } from "@/utils/passwordreset";
 
 
 export const generateVerificationToken = async (email: string) => {
@@ -24,4 +25,27 @@ export const generateVerificationToken = async (email: string) => {
   }).catch(error => console.log("\x1b[31m", 'DB VerificationToken Create Error: ', "\x1b[0m", error));
   console.log("\x1b[36m", 'GenerateVerificationToken new verificationToken: ', "\x1b[0m", verificationToken);
   return verificationToken;
+}
+
+export const generatePasswordResetToken = async (email: string) => {
+  let token = uuidv4();
+  let expires = new Date(new Date().getTime() + 3600 * 1000);
+
+  let existingToken = await getPasswordResetTokenByEmail(email);
+  console.log("\x1b[36m", 'GeneratePasswordResetToken ExistingToken: ', "\x1b[0m", existingToken);
+  if (existingToken) {
+    await db.passwordResetToken.delete({
+      where: { id: existingToken.id }
+    }).catch(error => console.log("\x1b[31m", 'GeneratePasswordResetToken DB PasswordResetToken Delete Error: ', "\x1b[0m", error));
+  }
+
+  let passwordResetToken = await db.passwordResetToken.create({
+    data: {
+      email,
+      token,
+      expires
+    }
+  }).catch(error => console.log("\x1b[31m", 'GeneratePasswordResetToken DB PasswordResetToken Create Error: ', "\x1b[0m", error));
+  console.log("\x1b[36m", 'GeneratePasswordResetToken new passwordResetToken: ', "\x1b[0m", passwordResetToken);
+  return passwordResetToken;
 }
