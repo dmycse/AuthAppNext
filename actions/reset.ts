49e81fafe0ce07276cef638485285357909cb4ea
dmycse <dmycse@gmail.com>
2024-09-08 14:59:53 +0300

    Refactor(actions): edit errors in reset action

--- actions/reset.ts
+++ actions/reset.ts
@@ -14,7 +14,7 @@ export const reset = async (formData: ResetFormType) => {
   let validatedResult = ResetFormSchema.safeParse(formData);
   console.log("\x1b[33m", 'ACTION Reset validatedResult: ', "\x1b[0m", validatedResult);
   if (!validatedResult.success) {
-    return {error: 'Invalid email. Try again.'};
+    return {error: 'Invalid email!'};
   }
 
   let { email } = validatedResult.data;
@@ -22,7 +22,7 @@ export const reset = async (formData: ResetFormType) => {
   let existingUser = await getUserByEmail(email);
   console.log("RESET ExistingUser: ", existingUser);
   if (!existingUser) {
-    return {error: "Email not found!"};
+    return {error: "User not found!"};
   }
 
   let passwordResetToken = await generatePasswordResetToken(existingUser.email);
