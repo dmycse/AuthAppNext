3d6e942da1f7f1b383c7647266e6c3fccd2f65e8
dmycse <dmycse@gmail.com>
2024-09-11 13:56:35 +0300

    Feat(app): create navbar in (protected) _components

--- app/(protected)/layout.tsx
+++ app/(protected)/layout.tsx
@@ -1,17 +1,24 @@
 import { ReactNode } from "react";
+import { SessionProvider } from "next-auth/react";
+import { auth } from "@/auth";
 import { Navbar } from "./_components/navbar";
+
 
 type ProtectedLayoutProps = {
   children: ReactNode
 }
 
-export default function ProtectedLayout({children}: ProtectedLayoutProps) {
+export default async function ProtectedLayout({children}: ProtectedLayoutProps) {
+  let session = await auth();
+
   return (
-    <div
-      className="w-full h-full flex flex-col items-center justify-center gap-y-10 bg-sky-500"
-    >
-      <Navbar />
-      {children}
-    </div>
+    <SessionProvider session={session}>
+      <div
+        className="w-full h-full flex flex-col items-center justify-center gap-y-10 bg-sky-500"
+      >
+        <Navbar />
+        {children}
+      </div>
+    </SessionProvider>
   )
 }
