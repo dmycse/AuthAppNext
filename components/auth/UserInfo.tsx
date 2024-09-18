803758626662318fdefb8cb0d6e443f988f8d3f1
dmycse <dmycse@gmail.com>
2024-09-18 11:22:58 +0300

    Refactor(components): refactor user data presentaion in UserInfo

--- components/auth/UserInfo.tsx
+++ components/auth/UserInfo.tsx
@@ -10,13 +10,13 @@ export const UserInfo =({ user, label }: UserInfoProps) => {
 
   console.log('UserInfo: ', user);
 
-  let userDetails = user && {
+  let userDetails = user && Object.entries({
     ID: user.id,
     Name: user.name,
     Email: user.email,
     Role: user.role,
     "2FA": user.isTwoFactorEnabled ? "Enabled" : "Disabled",
-  };
+  });
 
   return (
     <Card className="w-[600px] shadow-md">
@@ -26,7 +26,7 @@ export const UserInfo =({ user, label }: UserInfoProps) => {
         </p>
       </CardHeader>
       <CardContent className="space-y-4">
-        { userDetails && Object.entries(userDetails).map(([key, value]) => (
+        {userDetails?.map(([key, value]) => (
           <div key={key} className="p-3 flex justify-between items-center rounded-sm shadow-md">
             <p className="text-sm font-medium">{key}</p>
             <p className="p-2 text-md font-medium font-mono bg-slate-100 rounded-md truncate">
