647c601b3b1deb0436ae4ef75c753ab0382c793a
dmycse <dmycse@gmail.com>
2024-09-18 11:31:38 +0300

    Feat(actions): add logic to settings action

--- actions/settings.ts
+++ actions/settings.ts
@@ -1,40 +1,115 @@
 "use server";
 
 import { db } from "@/lib/db";
-import type { SettingsFormType } from "@/schemas";
+import { SettingsFormSchema, type SettingsFormType } from "@/schemas";
 
-import { getUserById } from "@/utils/user";
+import { getUserByEmail, getUserById } from "@/utils/user";
 import { authUser } from "@/utils/authuser";
 import { Prisma } from "@prisma/client";
-import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
+import { generateVerificationToken } from "@/lib/tokens";
+import { sendVerificationEmail } from "@/lib/mail";
+import { createPasswordHash, mathcUserPassword } from "@/utils/pass";
 
 
 export const settings = async (values: SettingsFormType) => {
-  console.log('ACTION Settings values: ', values);
+  console.log("\x1b[33m%s\x1b[0m",'ACTION Settings values: ', values);
   let currentUser = await authUser();
 
   if (!currentUser) {
     return {error: "User is unauthorized!"};
   }
 
-  // let name = values.get('name') as SettingsType['name'];
-
   let dbUser = await getUserById(currentUser.id as string);
 
   if (!dbUser) {
     return {error: "User not found!"};
   }
-  console.log('ACTION Settings dbUser: ', dbUser);
+
+  let validateValues = SettingsFormSchema.safeParse(values);
+  console.log("\x1b[33m%s\x1b[0m",'ACTION Settings validateValues: ', validateValues.data);
+  if (!validateValues.success) {
+    return {error: 'Invalid data. Try again.'};
+  }
+
+  let {name, email, password, newPassword, isTwoFactorEnabled, role} = validateValues.data;
+  
+  let dataForUpdate: SettingsFormType = {
+    name: name !== currentUser.name ? name : undefined,
+    role: role !== currentUser.role ? role : currentUser.role
+  };
+  
+  // * These logic below are for non-OAuth users
+  if (!currentUser.isOAuth) {
+
+      if (email && email !== currentUser.email) {
+        let userExists = await getUserByEmail(values.email as string);
+        
+        if (userExists && userExists.id !== currentUser.id) {
+          return {error: "Email is already taken!"};
+        }
+    
+        let verificationToken = await generateVerificationToken(email as string);
+
+        if (verificationToken) {
+          await sendVerificationEmail(verificationToken.email, verificationToken.token);
+          return {success: "Verification email sent!"};
+        }
+        else {
+          return {error: 'Something went wrong! Email has not been changed!'};
+        }
+      }
+
+      if (password && !newPassword) {
+        return {error: "Don't enter your password if you don't want to change it!"};
+      }
+
+      if (newPassword && !password) {
+        return {error: "Password is required!"};
+      }
+
+      if (password && newPassword && dbUser.password) {
+          let passwordMatch = await mathcUserPassword({email: dbUser.email, password});
+
+          if (!passwordMatch) {
+            return {error: "Incorrect password!"};
+          }
+
+          let hashedPassword = await createPasswordHash(newPassword);
+
+          if (!hashedPassword) {
+            return {error: "Password change cannot be done! Try again."};
+          }
+
+          dataForUpdate.password = hashedPassword;
+        }
+
+        dataForUpdate.isTwoFactorEnabled = isTwoFactorEnabled !== currentUser.isTwoFactorEnabled 
+                                            ? isTwoFactorEnabled 
+                                            : undefined;
+
+  }
+  
+  // These data below are not editable for OAuth users
+  // if (currentUser.isOAuth) {
+  //   values.name = values.name !== currentUser.name ? values.name : undefined;
+  //   values.email = undefined;
+  //   values.password = undefined;
+  //   values.newPassword = undefined;
+  //   values.isTwoFactorEnabled = undefined;
+  // }
+
+  console.log("\x1b[33m%s\x1b[0m", 'ACTION Settings dataForUpdate: ', dataForUpdate);
   await db.user.update({
     where: {id: dbUser.id},
-    data: {...values}
+    data: {...dataForUpdate}
   })
   .catch(error => {
     if (error instanceof Prisma.PrismaClientKnownRequestError) {
-      console.log('ACTION Settings DB User Update Error: ', error.code);
+      console.log("\x1b[31m%s\x1b[0m", 'ACTION Settings DB User Update Error: ', error.code);
     }
+    console.log("\x1b[31m%s\x1b[0m", 'ACTION Settings DB User Update Error: ', error);
     return {error: "Something went wrong!"}
   });
-
-  return {success: "Name updated!"};
+  
+  return {success: "Your data has been updated!"};
 }
