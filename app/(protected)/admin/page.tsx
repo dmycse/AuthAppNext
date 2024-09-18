390abe1f159197f58aa6211e40af7b23f052867e
dmycse <dmycse@gmail.com>
2024-09-18 11:27:27 +0300

    Refactor(app): refactor admin page

--- app/(protected)/admin/page.tsx
+++ app/(protected)/admin/page.tsx
@@ -1,24 +1,36 @@
 import { AdminOnlyTest } from "@/components/auth/AdminOnlyTest";
+import { FormError } from "@/components/auth/FormError";
 import { FormSuccess } from "@/components/auth/FormSuccess";
 import { RoleGate } from "@/components/auth/RoleGate";
 import { Card, CardHeader, CardContent } from "@/components/ui/card";
+import { authRole } from "@/utils/authuser";
 import { UserRole } from "@prisma/client";
 
 
 export default async function AdminPage() {
+  let role = await authRole();
+
   return (
     <Card className="w-[600px] shadow-md">
       <CardHeader>
         <p className="text-2xl font-semibold text-center">
-          Admin
+          Admin Panel
         </p>
       </CardHeader>
       <CardContent className="space-y-4">
-        <RoleGate allowedRole={UserRole.Admin}>
+        {/* <RoleGate allowedRole={role === UserRole.Admin}>
           <FormSuccess message="You are allowed to access this page!" />
-        </RoleGate>
-        <AdminOnlyTest label="API Route" type="api" />
-        <AdminOnlyTest label="Server Action" type="action" />
+        </RoleGate> */}
+        {role === UserRole.Admin 
+          ? <FormSuccess message="You are allowed to access this page!" />
+          : <FormError message="You are not allowed to access this page!" />
+        }
+        {role === UserRole.Admin && (
+          <>
+            <AdminOnlyTest label="API Route" type="api" />
+            <AdminOnlyTest label="Server Action" type="action" />
+          </>
+        )}
       </CardContent>  
     </Card>
   )
